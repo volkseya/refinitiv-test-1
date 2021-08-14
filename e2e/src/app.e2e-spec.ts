4c08d4c771a0eb0e01e4986cc3e0a209e1d693b3
volkseya <33420104+volkseya@users.noreply.github.com>
2021-08-14 15:46:47 +0700

    done

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('kstone-test app is running!');
+    expect(await page.getTitleText()).toEqual('refinitiv-test-1 app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kstone-test';
+  num: number;
+  method: string = 'isPrime';
+  result: boolean;
+
+  validateNum() {
+    console.log(this.num);
+    if (this.num < 0) {
+      this.num = 1;
+    } else if (!Number.isInteger(this.num)) {
+      this.num = Math.round(this.num);
+    }
+  }
+
+  isPrime(num) {
+    if(num <= 1){
+      return false;
+    }
+    for(let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i == 0) 
+      return false;
+    }
+    return true;
+  }
+
+  isFibonacci(num) {
+    // https://en.wikipedia.org/wiki/Fibonacci_number#Identification
+    return this.isPerfectSquare(5 * num * num + 4) || this.isPerfectSquare(5 * num * num - 4);
+  }
+
+  isPerfectSquare(num) {
+    return Math.sqrt(num) % 1 === 0;
+  }
+
+  handleCalculate() {
+    this.validateNum();
+    if (this.method === 'isPrime') {
+      this.result = this.isPrime(this.num);
+    } else if (this.method === 'isFibonacci') {
+      this.result = this.isFibonacci(this.num);
+    }
+  }
 }
